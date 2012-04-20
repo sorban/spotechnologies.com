78584fec8a6bdab7433fe731a0c28023f140799d
Stephen Orban <sorban@gmail.com>
2012-04-19 20:51:17 -0400

    port 8080

--- app/app.js
+++ app/app.js
@@ -34,6 +34,6 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000, function(){
+app.listen(8080, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
