2e22cdc9668cf550efc0f994d62de77620bf04db
sorban <sorban@gmail.com>
2012-04-27 16:37:57 -0300

    Update app/scripts/insertUser.js

--- app/scripts/insertUser.js
+++ app/scripts/insertUser.js
@@ -5,7 +5,14 @@ var db = mongoose.connect('mongodb://localhost/spotechnologiesdb');
 var User  = require('../schemas/User');
 
 var testUser = new User({ 
-          'username' : 'test1',
-          'password' : 'test1'});
-testUser.save();
-process.exit();
+          'username' : 'test2',
+          'password' : 'test2'});
+
+testUser.save(function(err) {
+  console.log('error: ' + err);
+  User.find(function(err, arr) {
+      console.log(arr);
+      console.log('length='+arr.length);
+      process.exit();
+  });
+});
